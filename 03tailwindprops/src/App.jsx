de270d3b0bc279f87aaca8e4ba5abe0d448cce77
manikanta9809 <varshinivarma16@gmail.com>
2024-12-29 07:16:35 +0530

    Added Inital files

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <>
+      <Card username="Manikanta" img='https://images.pexels.com/photos/825904/pexels-photo-825904.jpeg?auto=compress&cs=tinysrgb&w=800' />
+      <Card username="Dileep" />
+    </>
+  );
+}
+
+export default App;
