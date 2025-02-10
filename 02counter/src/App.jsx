2a0e69a181b05440c52e94aaf95e569a3dcb7225
manikanta201104 <manikanta0224402@gmail.com>
2025-02-10 19:23:12 +0530

    Remove unused Tailwind CSS files and configurations

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,51 +1,42 @@
-// import {useState} from "react";
-// import './App.css'
+import { useState } from 'react'
+import './App.css'
 
-// function App() {
-//   const [counter, setCounter] = useState(10);
-//   // let counter=15;
-
-//   const addValue=function(){
-//     setCounter(counter+1);
-//   }
-//   const removeValue=function(){
-//     setCounter(counter-1);
-//   }
-//   return (
-//     <>
-//      <h1>Learning React from Manikanta {counter}</h1>
-//      <h2>Counter Value:{counter}</h2>
-//      <button onClick={addValue}>Add Value</button>
-//      <button onClick={removeValue}>Remove Value</button>
-//      <p>footer:{counter}</p>
-//     </>
-//   )
-// }
-
-// export default App
-
-import {useState} from "react";
-import "./App.css"
 function App() {
-  const[counter,setCounter]=useState(10);
-  const addValue=function(){
-    setCounter((prevCounter)=>prevCounter+1);//these lines are actually updating the value which is the previous updated value and plus one to it
-    setCounter((prevCounter)=>prevCounter+1);
-    setCounter((prevCounter)=>prevCounter+1);
-    setCounter((prevCounter)=>prevCounter+1);
+  const [counter, setCounter] = useState(10)
 
+  // let counter=17;
+
+  // const addValue=()=>{
+  //   setCounter(counter+1);
+  //   console.log(counter);
+  // }
+
+  //interview question create a counter 
+  const addValue=()=>{
+    setCounter((prevCounter)=> prevCounter+100);
+    setCounter((prevCounter)=> prevCounter+100);
+    setCounter((prevCounter)=> prevCounter+100);
+    setCounter((prevCounter)=> prevCounter+100);
+
+    console.log(counter);
   }
-  const removeValue=function(){
-    setCounter(counter-1);
+  const removeValue=()=>{
+    // setCounter(counter-1);
+    setCounter((prevCounter)=> prevCounter-100);
+    setCounter((prevCounter)=> prevCounter-100);
+    setCounter((prevCounter)=> prevCounter-100);
+    setCounter((prevCounter)=> prevCounter-100);
+    console.log(counter)
   }
   return (
     <>
-      <h1>Learning react from Manikanta {counter}</h1>
-      <h2>Counter Value:{counter}</h2>
-      <button onClick={addValue}>Add Value</button>
+      <h1>This is Manikanta from react {counter}</h1>
+      <h2>Counter value:{counter}</h2>
+      <button onClick={addValue}>Add value</button> {""}
       <button onClick={removeValue}>Remove Value</button>
-      <p>footer:{counter}</p>
+      <h3>Footer:{counter}</h3>
     </>
   )
 }
-export default App;
+
+export default App
